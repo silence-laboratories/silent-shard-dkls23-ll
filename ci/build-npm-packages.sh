175e99eb04de7d371e93088d5ebbe0f32a00ed27
Artem Mironov <artem.mironov@gmail.com>
2024-02-19 10:32:00 +0100

    Reproducible wasm builds
    - added Cargo.lock
    - added ci/build-npm-packages.sh
    - added Dockerfile.wasm
    - bumped version to 1.0.1
    - update Cargo.license

---
+++ ci/build-npm-packages.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -e
+
+ver=""
+scope="@silencelaboratories"
+
+usage() {
+    echo "build-npm-packages.sh [ -v version ] [ -s scope ]";
+    exit 1
+}
+
+while getopts "v:s:" opt; do
+    case "${opt}" in
+        v)
+            ver=${OPTARG}
+            ;;
+        s)
+            scope=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ -z "${ver}" ]; then
+    echo 'pass -v "version"'
+    exit 1
+fi
+
+
+build() {
+    local suffix="$1"
+
+    wasm-pack build \
+          -t web \
+          -d pkg-${suffix} \
+          --out-name dkls-wasm-ll-${suffix} \
+          wrapper/wasm-ll
+
+          jq ".name=\"${scope}/dkls-wasm-ll-${suffix}\" | .version=\"${ver}\" | .license=\"SLL\"" \
+            < wrapper/wasm-ll/pkg-${suffix}/package.json \
+            > wrapper/wasm-ll/pkg-${suffix}/package.json.new
+
+           mv wrapper/wasm-ll/pkg-${suffix}/package.json.new \
+              wrapper/wasm-ll/pkg-${suffix}/package.json
+}
+
+build "web"
+build "node"
